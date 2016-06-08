b38e231c4d274543cc04c01fa4c06bcc8080f0b2
szmoro <moro2694@gmail.com>
2016-06-08 14:34:08 +0200

    working with ajax

--- prova.php
+++ prova.php
@@ -1,21 +1,28 @@
 <?php
-    $servername = "localhost";
-    $username = "szmoro";
-    $password = "password";
+$servername = "localhost";
+$username = "szmoro";
+$password = "Francona";
+$dbname = "dbtim";
 
-    $conn = new mysqli($servername, $username, $password);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } 
-    // Create database
-    $sql = "CREATE DATABASE myDB";
-    if ($conn->query($sql) === TRUE) {
-        echo "Database created successfully";
-    } else {
-        echo "Error creating database: " . $conn->error;
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT p.id_prod, p.name, p.price, c.name 
+        FROM products p JOIN product_categories c
+        WHERE p.id_prod = 1 AND p.id_prod_cat = c.id_prod_cat";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id_prod"]. " - Name: " . $row["name"]. " " . $row["price"]. $row["name"]."<br>";
     }
-    
-  
-    $conn->close();
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
