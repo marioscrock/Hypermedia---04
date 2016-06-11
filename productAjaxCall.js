972248529c34d043256c44a4b0534cf026a9d7d4
szmoro <moro2694@gmail.com>
2016-06-11 18:05:57 +0200

    Start working on productData ajax call

---
+++ productAjaxCall.js
@@ -0,0 +1,29 @@
+$(document).ready(ready);
+
+function ready(){
+    console.log("I'm ready!");
+    var id=1;
+    $(".loadmore").on("click",loadMoreClicked);
+
+    $.ajax({
+        method: "POST",
+        dataType: "json", //type of data
+        crossDomain: true, //localhost purposes
+        url: "productData.php", //Relative or absolute path to file.php file
+        data: {course:id},
+        success: function(response) {
+            var courses=JSON.parse(response);
+            var el="";
+            for(var i=0;i<courses.length;i++){
+                console.log(courses[i].title);      
+                el+="<div class='course' id='c"+courses[i].id+"'><h2>"+courses[i].title+"</h2><span>"+courses[i].description+"</span></div>";               
+            }
+            $("body").html(el);
+        },
+        error: function(request,error) 
+        {
+            console.log("Error");
+        }
+    });
+
+}
