1a988cc2d2673cf8f74c50cbaa8f2f9f6a697763
szmoro <moro2694@gmail.com>
2016-06-15 15:50:44 +0200

    working on ajax calls

--- productAjaxCall.js
+++ productAjaxCall.js
@@ -2,23 +2,23 @@ $(document).ready(ready);
 
 function ready(){
     console.log("I'm ready!");
-    var id=1;
-    $(".loadmore").on("click",loadMoreClicked);
-
+		var id = 1;
     $.ajax({
         method: "POST",
         dataType: "json", //type of data
         crossDomain: true, //localhost purposes
         url: "productData.php", //Relative or absolute path to file.php file
-        data: {course:id},
+        data: {id:id},
         success: function(response) {
-            var courses=JSON.parse(response);
-            var el="";
+						console.log(response);
+            //var courses=JSON.parse(response);
+						//console.log(courses);
+            /*var el="";
             for(var i=0;i<courses.length;i++){
                 console.log(courses[i].title);      
                 el+="<div class='course' id='c"+courses[i].id+"'><h2>"+courses[i].title+"</h2><span>"+courses[i].description+"</span></div>";               
             }
-            $("body").html(el);
+            $("body").html(el);*/
         },
         error: function(request,error) 
         {
--- productData.php
+++ productData.php
@@ -4,8 +4,7 @@
 //echo "I'm the php";
 
 //connection to db
-$mysqli = new mysqli("localhost", "root", "", "biggym");
-
+$mysqli = new mysqli("localhost", "root", "", "dbtim");
 if (mysqli_connect_errno()) { //verify connection
     echo "Error to connect to DBMS: ".mysqli_connect_error(); //notify error
     exit(); //do nothing else 
@@ -14,31 +13,25 @@ else {
     //echo "Successful connection"; // connection ok
 
     # extract results mysqli_result::fetch_array
-    $query = " SELECT * FROM course  ";
+    $query = " SELECT * FROM products WHERE id_prod = 1  ";
     //query execution
     $result = $mysqli->query($query);
     //if there are data available
-    if($result->num_rows >0)
-    {
-        $myArray = array();//create an array
-        while($row = $result->fetch_array(MYSQL_ASSOC)) {
+		if($result!=FALSE){
+			if($result->num_rows >0)
+			{
+				$myArray = array();//create an array
+				 while($row = $result->fetch_array(MYSQL_ASSOC)) {
             $myArray[] = $row;
         }
-        echo json_encode($myArray);
-    }
-
+				echo json_encode($myArray);
+			}
+		}
     //free result
     $result->close();
 
     //close connection
     $mysqli->close();
-
-
-
 }
-
-
-
-
 
 ?>
