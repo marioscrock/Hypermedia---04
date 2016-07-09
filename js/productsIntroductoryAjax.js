73aeebcc5fb00cf46c7e6b3df0d454fe57df57c9
szmoro <moro2694@gmail.com>
2016-07-09 17:38:21 +0200

    added item in products introductory

--- js/productsIntroductoryAjax.js
+++ js/productsIntroductoryAjax.js
@@ -16,12 +16,20 @@ function ready() {
 		url: "/php/productsIntroductoryData.php", //Relative or absolute path to file.php file
 		data: {
 			id: GET["category"]
+			, item: GET["item"]
 		}
 		, success: function (response) {
 			console.log(response[0].name);
 			var tiles = "";
-			for (var i = 0; i < response.length; i++) {
-				tiles += "<div class=\x22col-xs-12 col-sm-6 col-md-4 col-lg-3 tileContainer\x22>	<div class=\x22container-fluid tile\x22>	<img src=\x22" + response[i].img + "\x22 class=\x22img-responsive\x22 alt=\x22Generic placeholder thumbnail 					\x22>	 <div class=\x22item\x22><p>" + response[i].name + "</p></div> <div class=\x22item itemPrice\x22><p>" + response[i].price + "€</p></div>	<div class=\x22item \x22><button type =\x22button\x22 id=\x22btn-detail\x22 class=\x22btn btn-primary\x22 onclick=\x22location.href = 'product.html?id_prod=" + response[i].id_prod + "'\x22 > Dettagli </button></div> </div> </div>";
+			if (GET["item"] === "product_categories") {
+				for (var i = 0; i < response.length; i++) {
+					tiles += "<div class=\x22col-xs-12 col-sm-6 col-md-4 col-lg-3 tileContainer\x22>	<div class=\x22container-fluid tile\x22>	<img src=\x22" + response[i].img + "\x22 class=\x22img-responsive\x22 alt=\x22Generic placeholder thumbnail \x22>	 <div class=\x22item\x22><p>" + response[i].name + "</p></div> <div class=\x22item itemPrice\x22><p>" + response[i].price + "€</p></div>	<div class=\x22item \x22><button type =\x22button\x22 id=\x22btn-detail\x22 class=\x22btn btn-primary\x22 onclick=\x22location.href = 'product.html?id_prod=" + response[i].id + "'\x22 > Dettagli </button></div> </div> </div>";
+				}
+			}
+			else {
+				for (var i = 0; i < response.length; i++) {
+					tiles += "<div class=\x22col-xs-12 col-sm-6 col-md-4 col-lg-3 tileContainer\x22>	<div class=\x22container-fluid tile\x22>	<img src=\x22" + response[i].img + "\x22 class=\x22img-responsive\x22 alt=\x22Generic placeholder thumbnail \x22>	 <div class=\x22item\x22><p>" + response[i].name + "</p></div> <div class=\x22item \x22><button type =\x22button\x22 id=\x22btn-detail\x22 class=\x22btn btn-primary\x22 onclick=\x22location.href = 'product.html?id_prod=" + response[i].id + "'\x22 > Dettagli </button></div> </div> </div>";
+				}
 			}
 			$("#tiles").html(tiles);
 			$("#title").html("<img class='titleIcon' src=" + response[0].category_img + "> <h1 class='sectionTitle'> " + response[0].category.toUpperCase() + "</h1>");
--- php/productsIntroductoryData.php
+++ php/productsIntroductoryData.php
@@ -19,8 +19,11 @@ else {
 		/* change character set to utf8 */
 		$mysqli->set_charset("utf8");
     
-		
-    $query = "SELECT products.id_prod, products.name, products.price, images.img, product_categories.name as category, product_categories.img as category_img FROM product_images JOIN products JOIN images JOIN product_categories WHERE(images.id_img = product_images.id_img)&&(products.id_prod=product_images.id_prod)&&(products.id_prod_cat=".$_POST["id"].")&&(product_categories.id_cat =products.id_prod_cat) GROUP BY products.id_prod";
+		if($_POST["item"] === "product_categories"){
+    $query = "SELECT products.id_prod as id, products.name, products.price, images.img, product_categories.name as category, product_categories.img as category_img FROM product_images JOIN products JOIN images JOIN product_categories WHERE(images.id_img = product_images.id_img)&&(products.id_prod=product_images.id_prod)&&(products.id_prod_cat=".$_POST["id"].")&&(product_categories.id_cat =products.id_prod_cat) GROUP BY products.id_prod";
+		} else {
+			 $query = "SELECT services.id_serv as id, services.name, services.img_bot as img, service_categories.name as category, service_categories.img as category_img FROM services JOIN service_categories WHERE (services.id_cat_serv=".$_POST["id"].")&&(service_categories.id_cat = services.id_cat_serv) GROUP BY services.id_serv";
+		}
     
 		//query execution
     $result = $mysqli->query($query);
