48e172eb144ec9d065a1a376e99626d729cf892c
mario_scrock <mario.scrocca@gmail.com>
2016-07-10 12:03:12 +0200

    add highlights assistenza ajax call

---
+++ php/highlights.php
@@ -0,0 +1,37 @@
+<?php
+
+header('Content-type: text/plain; charset=utf-8');
+
+$mysqli = new mysqli("localhost", "root", "", "dbtim");
+
+if (mysqli_connect_errno()) { //verify connection
+    echo "Error to connect to DBMS: ".mysqli_connect_error(); //notify error
+    exit(); //do nothing else 
+}
+else {
+    
+    $mysqli->set_charset("utf8");
+    
+    $query = "SELECT id_ass, title, category FROM assistances, assistance_subcategories WHERE highlights=1 && subcategory = id_ass_subcat";
+  
+    $result = $mysqli->query($query);
+		if($result==FALSE) {
+			echo "no result";
+		} else {
+			if($result->num_rows >0)
+			{
+				$array = array();
+				while($row = $result->fetch_assoc()) {
+					array_push($array, $row);
+				}
+				echo json_encode($array);
+			}
+		}
+	
+    $result->close();
+
+    $mysqli->close();
+  
+}
+
+?>
