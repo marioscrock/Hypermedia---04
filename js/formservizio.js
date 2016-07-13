967835d57505f1b714a83e5680e35b054b4ccc79
Alessandro <a.polenghi994@gmail.com>
2016-07-13 20:07:30 +0200

    bottone delle form servizi

--- js/formservizio.js
+++ js/formservizio.js
@@ -36,40 +36,17 @@ function ready() {
             id: GET["id"]
         }
         , success: function (response) {
-            orientationSecond = "<li><a href='introductory.html?category=" + response[0].category + "&item=service_categories'>" + response[0].category_name + "</a></li><li class='active'>" + response[0].name + "</li>";
+            orientationSecond = "<li><a href='introductory.html?category=" + response[0].category + "&item=service_categories'>" + response[0].category_name + "</a></li><li><a href='servizio.html?id=" + response[0].id_serv + "&item=service_categories'>" + response[0].name + "</a></li><li class='active'>Form di sottoscrizione del servizio " + response[0].name + "</li>";
+
             $(".sectionTitle").html(response[0].name);
-            $("#imgContainerTop").prepend("<img class='img-responsive' src=" + response[0].img_top + ">");
             $("#servizioss").html(response[0].name);
-            $("#imgContainerBot").prepend("<img class='img-responsive' src=" + response[0].img_bot + ">");
-            $("#presentation").html(response[0].par_top);
-            $("#specifications").html(response[0].par_bot);
 
-            $("#btn-form").append("<button type =\x22button\x22 class=\x22btn btn-primary\x22 onclick=\x22location.href = 'form-servizi.html?id=" + response[0].id_serv + "'\x22 >Sottoscrivi Servizio<br> Maggiori Informazioni</button>");
         }
         , error: function (request, error) {
             console.log("Error");
         }
     });
-    $.ajax({
-        method: "POST"
-        , dataType: "json", //type of data
-        crossDomain: true, //localhost purposes
-        url: "/php/serviceFAQ.php", //Relative or absolute path to file.php file
-        data: {
-            id: GET["id"]
-        }
-        , success: function (response) {
-            if (response[0].answer.length >= 0) {
-                var button = "";
-                button = "<div><button type =\x22button\x22 id=\x22btn-detail\x22 class=\x22btn btn-primary\x22 onclick=\x22location.href = 'FAQ.html?id=" + response[0].id_serv + "'\x22 > FAQ </button></div>";
-            }
 
-            $("#optionalFAQBUTTON").append(button);
-        }
-        , error: function (request, error) {
-            console.log("Error");
-        }
-    });
     $(document).ajaxStop(function () {
         $("#orientation").html(orientationFirst + orientationSecond);
     });
