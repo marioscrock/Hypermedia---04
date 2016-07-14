a31aea2d596ccb85ba9751120c21ac750655cd71
mario_scrock <mario.scrocca@gmail.com>
2016-07-14 12:54:25 +0200

    matchHeight

--- js/assistanceIntroductory.js
+++ js/assistanceIntroductory.js
@@ -8,56 +8,63 @@ function ready() {
     var param = query[i].split("=");
     GET[decodeURIComponent(param[0])] = decodeURIComponent(param[1] || "");
   }
-  $.ajax({
-    method: "POST"
-    , dataType: "json", //type of data
-    crossDomain: true, //localhost purposes
-    url: "/php/categoriesIntrodData.php", //Relative or absolute path to file.php file
-    data: {
-      id: GET["item"]
-    }
-    , success: function (response) {
-      for (var i = 0; i < response.length; i++) {
-        if (response[i].id_cat === GET["category"]) {
-          $("#titleCategory").html("<img class='titleIcon' src='" + response[i].img + "'><h1 class='sectionTitle'>" + response[i].name + "</h1>");
-          document.title = "Assistenza - " + response[i].name;
-          break;
-        }
+
+  $.when(
+    $.ajax({
+      method: "POST"
+      , dataType: "json", //type of data
+      crossDomain: true, //localhost purposes
+      url: "/php/categoriesIntrodData.php", //Relative or absolute path to file.php file
+      data: {
+        id: GET["item"]
       }
-    }
-    , error: function (request, error) {
-      console.log("Error");
-    }
-  });
-  $.ajax({
-    method: "POST"
-    , dataType: "json", //type of data
-    crossDomain: true, //localhost purposes
-    url: "/php/assistanceIntroductoryData.php", //Relative or absolute path to file.php file
-    data: {
-      id: GET["category"]
-    }
-    , success: function (response) {
-      var subcategories = [];
-      var subcategory = "";
-      for (var i = 0; i < response.length; i++) {
-        if (!($.inArray(response[i].name, subcategories) > -1)) {
-          subcategories.push(response[i].name);
-        }
-      }
-      for (var i = 0; i < subcategories.length; i++) {
-        subcategory += "<div class='col-xs-12 col-sm-6 col-md-6 col-lg-4'><ul class='custom-bullet'><h2>" + subcategories[i].toUpperCase() + "</h2>";
-        for (var j = 0; j < response.length; j++) {
-          if (response[j].name === subcategories[i]) {
-            subcategory += "<li><a href='assistenza.html?id=" + response[j].id_ass + "'>" + response[j].title + "</a></li>";
+      , success: function (response) {
+        for (var i = 0; i < response.length; i++) {
+          if (response[i].id_cat === GET["category"]) {
+            $("#titleCategory").html("<img class='titleIcon' src='" + response[i].img + "'><h1 class='sectionTitle'>" + response[i].name + "</h1>");
+            document.title = "Assistenza - " + response[i].name;
+            break;
           }
         }
-        subcategory += "</ul></div>";
       }
-      $("#subcategories").html(subcategory);
-    }
-    , error: function (request, error) {
-      console.log("Error");
-    }
-  });
+      , error: function (request, error) {
+        console.log("Error");
+      }
+    })
+    , $.ajax({
+      method: "POST"
+      , dataType: "json", //type of data
+      crossDomain: true, //localhost purposes
+      url: "/php/assistanceIntroductoryData.php", //Relative or absolute path to file.php file
+      data: {
+        id: GET["category"]
+      }
+      , success: function (response) {
+        var subcategories = [];
+        var subcategory = "";
+        for (var i = 0; i < response.length; i++) {
+          if (!($.inArray(response[i].name, subcategories) > -1)) {
+            subcategories.push(response[i].name);
+          }
+        }
+        for (var i = 0; i < subcategories.length; i++) {
+          subcategory += "<div class='col-xs-12 col-sm-6 col-md-6 col-lg-4 same-height'><ul class='custom-bullet'><h2>" + subcategories[i].toUpperCase() + "</h2>";
+          for (var j = 0; j < response.length; j++) {
+            if (response[j].name === subcategories[i]) {
+              subcategory += "<li><a href='assistenza.html?id=" + response[j].id_ass + "'>" + response[j].title + "</a></li>";
+            }
+          }
+          subcategory += "</ul></div>";
+        }
+        $("#subcategories").html(subcategory);
+      }
+      , error: function (request, error) {
+        console.log("Error");
+      }
+    })
+  ).then(function () {
+    $(function () {
+      $(".same-height").matchHeight();
+    });
+  })
 }
